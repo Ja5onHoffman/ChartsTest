e290c22b83aa23735251a209a8db64204b6b3f99
Jason <jasonandrewhoffman@gmail.com>
2022-01-12 16:37:40 -0600

    rename and build from this project instead of powercompare

---
+++ ChartsTest/DeviceGridHeader.swift
@@ -0,0 +1,19 @@
+//
+//  DeviceGridHeader.swift
+//  ChartsTest
+//
+//  Created by Jason Hoffman on 12/20/21.
+//
+
+import SwiftUI
+
+struct DeviceGridHeader: View {
+    
+    var body: some View {
+        HStack {
+            Text("Device 1")
+            Spacer()
+            Text("Device 2")
+        }
+    }
+}
